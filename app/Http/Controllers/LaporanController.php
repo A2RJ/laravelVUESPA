04ef71427f9d447369a179e1e2782d45737fd71f
a2rj <xcz.ardiansyahputra2468@gmail.com>
2020-08-20 23:34:51 +0700

    Function LPD

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -49,4 +49,40 @@ class LaporanController extends Controller
         @Untuk Halaman Laporan Posisi Keuangan
     **/
 
+    /** 
+        @Untuk Halaman Laporan Perubahan Dana
+    **/
+    public function LPD()
+    {
+        return response()->json([
+            'entitas' => Jurnal::where('no_akun', 40)->sum('jum_debet'),
+            'individual' => Jurnal::where('no_akun', 41)->sum('jum_debet'),
+            'penempatan' => Jurnal::where('no_akun', 42)->sum('jum_debet'),
+            'bagianAmilzakat' => Jurnal::where('no_akun', 43)->sum('jum_debet'),
+            'fakir' => Jurnal::where('no_akun', 44)->sum('jum_debet'),
+            'riqab' => Jurnal::where('no_akun', 45)->sum('jum_debet'),
+            'gharim' => Jurnal::where('no_akun', 46)->sum('jum_debet'),
+            'muallaf' => Jurnal::where('no_akun', 47)->sum('jum_debet'),
+            'sabilillah' => Jurnal::where('no_akun', 48)->sum('jum_debet'),
+            'ibnuSabil' => Jurnal::where('no_akun', 49)->sum('jum_debet'),
+            'muqayyadahKas' => Jurnal::where('no_akun', 50)->sum('jum_debet'),
+            'mutlaqahKas' => Jurnal::where('no_akun', 51)->sum('jum_debet'),
+            'bagianAmilInfak' => Jurnal::where('no_akun', 52)->sum('jum_debet'),
+            'pengelolaan' => Jurnal::where('no_akun', 53)->sum('jum_debet'),
+            'muqayyadahKredit' => Jurnal::where('no_akun', 54)->sum('jum_debet'),
+            'mutlaqahKredit' => Jurnal::where('no_akun', 55)->sum('jum_debet'),
+            'alokasi' => Jurnal::where('no_akun', 56)->sum('jum_debet'),
+            'amilzakat' => Jurnal::where('no_akun', 57)->sum('jum_debet'),
+            'amilinfak' => Jurnal::where('no_akun', 58)->sum('jum_debet'),
+            'penerimaan' => Jurnal::where('no_akun', 59)->sum('jum_debet'),
+            'bebanPegawai' => Jurnal::where('no_akun', 60)->sum('jum_debet'),
+            'bebanPenyusutan' => Jurnal::where('no_akun', 61)->sum('jum_debet'),
+            'bebanUmum' => Jurnal::where('no_akun', 61)->sum('jum_debet'),
+            'bungaBank' => Jurnal::where('no_akun', 62)->sum('jum_debet'),
+            'giro' => Jurnal::where('no_akun', 63)->sum('jum_debet'),
+            'nonalalLainnya' => Jurnal::where('no_akun', 64)->sum('jum_debet'),
+            'penggunaanNonhalal' => Jurnal::where('no_akun', 65)->sum('jum_debet')
+        ], Response::HTTP_OK);
+    }
+
 }
