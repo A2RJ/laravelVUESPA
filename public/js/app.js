ad511694d6493fbc882ea4b59605e47b2184242e
a2rj <xcz.ardiansyahputra2468@gmail.com>
2020-10-13 19:49:29 +0700

    new theme

--- public/js/app.js
+++ public/js/app.js
@@ -59242,8 +59242,8 @@ var app = new vue__WEBPACK_IMPORTED_MODULE_0___default.a({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/a2rj/Programming/Web/UIUX/cobaPurple/laravelVUESPA/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/a2rj/Programming/Web/UIUX/cobaPurple/laravelVUESPA/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/a2rj/Programming/Web/LARAVEL/laravelVUESPA/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/a2rj/Programming/Web/LARAVEL/laravelVUESPA/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
